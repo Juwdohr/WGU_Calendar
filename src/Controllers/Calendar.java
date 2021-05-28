a8582b3446ba3595aebac29ec9b239a3cf6a3c0f
Juwdohr <juwdohr@outlook.com>
2021-05-27 23:29:09 -0600

    * Created login feature   - Sets username to the usernameLbl

--- src/Controllers/Calendar.java
+++ src/Controllers/Calendar.java
@@ -1,5 +1,10 @@
 package Controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import Models.User;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -10,6 +15,14 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class Calendar {
+
+    private User user;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private MenuItem newAppointmentMenuItem;
@@ -28,6 +41,9 @@ public class Calendar {
 
     @FXML
     private MenuItem aboutMenuItem;
+
+    @FXML
+    private Label usernameLbl;
 
     @FXML
     private MenuButton addMenuButton;
@@ -65,4 +81,35 @@ public class Calendar {
     @FXML
     private Label errorLabel;
 
+    @FXML
+    void initialize() {
+        assert newAppointmentMenuItem != null : "fx:id=\"newAppointmentMenuItem\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert newCustomerMenuItem != null : "fx:id=\"newCustomerMenuItem\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert logoutMenuItem != null : "fx:id=\"logoutMenuItem\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert editAppointmentMenuItem != null : "fx:id=\"editAppointmentMenuItem\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert editCustomerMenuItem != null : "fx:id=\"editCustomerMenuItem\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert aboutMenuItem != null : "fx:id=\"aboutMenuItem\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert usernameLbl != null : "fx:id=\"usernameLbl\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert addMenuButton != null : "fx:id=\"addMenuButton\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert addAppointmentMenuItem != null : "fx:id=\"addAppointmentMenuItem\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert addCustomerMenuItem != null : "fx:id=\"addCustomerMenuItem\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert updateMenuButton != null : "fx:id=\"updateMenuButton\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert updateAppointmentMenuItem != null : "fx:id=\"updateAppointmentMenuItem\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert updateCustomerMenuItem != null : "fx:id=\"updateCustomerMenuItem\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert calendarGrid != null : "fx:id=\"calendarGrid\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert customerListView != null : "fx:id=\"customerListView\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert loadingLbl != null : "fx:id=\"loadingLbl\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert x3 != null : "fx:id=\"x3\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert x4 != null : "fx:id=\"x4\" was not injected: check your FXML file 'Calendar.fxml'.";
+        assert errorLabel != null : "fx:id=\"errorLabel\" was not injected: check your FXML file 'Calendar.fxml'.";
+    }
+
+    //Used to pass in user, and gather other data
+    public void initializeData(User user) {
+        this.user = user;
+        usernameLbl.setText(this.user.getUsername());
+    }
 }
+
+
+
--- src/Controllers/Login.java
+++ src/Controllers/Login.java
@@ -1,22 +1,28 @@
 package Controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import Models.User;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.time.ZoneId;
+import java.util.NoSuchElementException;
 
 public class Login {
 
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
+    Stage stage;
+    Parent scene;
 
     @FXML
     private Text programTitle;
@@ -40,13 +46,52 @@ public class Login {
     private Text errorMessageTxt;
 
     @FXML
-    void CloseApplication(MouseEvent event) {
-
+    void CloseApplication() {
+        stage = (Stage) closeBtn.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
-    void LoginUser(MouseEvent event) {
+    void LoginUser() throws IOException {
+        //TODO: Check user table in DB for user
+        try {
+            //TODO: iff A user is in DB then, create new root scene and pass user
+            //TODO: Log successful login
+            User user = getUser(usernameTxtfield.getText(), passwordTxtfield.getText());
 
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("../Views/Calendar.fxml"));
+            Parent newRoot = loader.load();
+
+            Calendar controller = loader.getController();
+            controller.initializeData(user);
+
+            Stage newStage = new Stage();
+            newStage.setScene(new Scene(newRoot));
+            newStage.setTitle("Calendar");
+            newStage.setResizable(true);
+            newStage.initStyle(StageStyle.DECORATED);
+
+            Stage currentStage = (Stage) loginBtn.getScene().getWindow();
+            currentStage.close();
+
+            newStage.show();
+        } catch (NoSuchElementException ex) {
+            errorMessageTxt.setText("Username/Password is incorrect");
+        }
+
+        //TODO: Close current scene if user is found in DB
+        //TODO: If A user is not in DB then, send error to scene
+        //TODO: Log unsuccessful login attempt
+    }
+
+    private User getUser(String username, String password) {
+        //TODO: Call to DB to get user
+        // if User exists pass back user,
+        // else throw an error
+        User user = new User();
+        user.setUsername(username);
+        return user;
     }
 
     @FXML
@@ -58,6 +103,38 @@ public class Login {
         assert loginBtn != null : "fx:id=\"loginBtn\" was not injected: check your FXML file 'Login.fxml'.";
         assert closeBtn != null : "fx:id=\"closeBtn\" was not injected: check your FXML file 'Login.fxml'.";
         assert errorMessageTxt != null : "fx:id=\"errorMessageTxt\" was not injected: check your FXML file 'Login.fxml'.";
+        errorMessageTxt.setText("");
+        //TODO: Internationalize programTitle
+        programTitle.setText("Calendar");
+        zoneIdLbl.setText(ZoneId.systemDefault().toString());
 
+        //Allows a user press the enter key to login to application.
+        usernameTxtfield.addEventHandler(KeyEvent.KEY_PRESSED, ev-> {
+            if(ev.getCode() == KeyCode.ENTER) {
+                try{
+                    LoginUser();
+                } catch(IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        passwordTxtfield.addEventHandler(KeyEvent.KEY_PRESSED, ev-> {
+            if(ev.getCode() == KeyCode.ENTER) {
+                try{
+                    LoginUser();
+                } catch(IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        loginBtn.addEventHandler(KeyEvent.KEY_PRESSED, ev-> {
+            if(ev.getCode() == KeyCode.ENTER) {
+                try{
+                    LoginUser();
+                } catch(IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
     }
 }
